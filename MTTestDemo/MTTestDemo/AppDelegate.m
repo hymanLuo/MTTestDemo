1798ab20fae0d0323f55e5c3c50a44e5e5379c88
hyman_lou <184507163@qq.com>
2019-01-15 22:36:54 +0800

    updata AppDelegete

--- MTTestDemo/MTTestDemo/AppDelegate.m
+++ MTTestDemo/MTTestDemo/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //test1 --hyman
     return YES;
 }
 
