1ad45546727b20278d982c43aceae783df32ebcf
hyman_lou <184507163@qq.com>
2019-01-15 22:42:12 +0800

    add MTTest

---
+++ MTTestDemo/MTTestDemo/MTTest.h
@@ -0,0 +1,17 @@
+//
+//  MTTest.h
+//  MTTestDemo
+//
+//  Created by cheng luo on 2019/1/15.
+//  Copyright © 2019年 homeSalf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MTTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MTTestDemo/MTTestDemo/MTTest.m
@@ -0,0 +1,13 @@
+//
+//  MTTest.m
+//  MTTestDemo
+//
+//  Created by cheng luo on 2019/1/15.
+//  Copyright © 2019年 homeSalf. All rights reserved.
+//
+
+#import "MTTest.h"
+
+@implementation MTTest
+
+@end
